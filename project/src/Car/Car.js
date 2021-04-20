0c8cbaddc585dc52db379748211b2f94d06863fd
Nikolay Dimitriev <dimitrievk56@mail.ru>
2021-04-20 13:57:45 +0300

    Стилизация компонентов: динамические классы, добавление классов через js

--- project/src/Car/Car.js
+++ project/src/Car/Car.js
@@ -1,12 +1,30 @@
 import './Car.css'
 
-const car = props => (
-    <div className="Car">
-        <h2>Car name: {props.name}</h2>
-        <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" value={props.name} onChange={props.onChangeName}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-);
+const car = props => {
+    const inputClasses = ['input'];
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+    return (
+        <div className="Car">
+            <h2>Car name: {props.name}</h2>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input 
+                type="text" 
+                value={props.name} 
+                onChange={props.onChangeName}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
 
 export default car;
