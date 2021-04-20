3aa0dd0e39ca51406905740dca634b8909527bc1
Nikolay Dimitriev <dimitrievk56@mail.ru>
2021-04-20 13:44:44 +0300

    Стилизация компонентов: подключение css файла

--- project/src/Car/Car.js
+++ project/src/Car/Car.js
@@ -1,12 +1,7 @@
+import './Car.css'
+
 const car = props => (
-    <div style={{
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-        display: 'block',
-        padding: '10px',
-        marginBottom: '10px',
-        boxShadow: '0px 4px 5px 0px rgba( 0, 0, 0, .14)'
-    }}>
+    <div className="Car">
         <h2>Car name: {props.name}</h2>
         <p>Year: <strong>{props.year}</strong></p>
         <input type="text" value={props.name} onChange={props.onChangeName}/>
