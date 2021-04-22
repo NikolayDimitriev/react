46925e1ccad914ba2db5a196324c6d926fe46e4c
Nikolay Dimitriev <dimitrievk56@mail.ru>
2021-04-22 17:09:35 +0300

    Компоненты React: Фрагменты 2. Сделали возврат без div, с R.Fragment и со своим High order component

--- project/src/Counter/Counter.js
+++ project/src/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Auxiliary from '../hoc/Auxiliary.js';
 
 class Counter extends React.Component {
     state = {
@@ -6,11 +7,21 @@ class Counter extends React.Component {
     }
 
     render() {
-        return [
-            <h2 key={'1'}>Counter: {this.state.counter}</h2>,
-            <button key={'2'} onClick={() => this.setState({counter: this.state.counter + 1})}>+</button>,
-            <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
-        ]
+        // return (
+        //     <React.Fragment>
+        //         <h2 key={'1'}>Counter: {this.state.counter}</h2>
+        //         <button key={'2'} onClick={() => this.setState({counter: this.state.counter + 1})}>+</button>
+        //         <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+        //     </React.Fragment>
+        // )
+        // Второй вариант
+        return (
+            <Auxiliary>
+                <h2 key={'1'}>Counter: {this.state.counter}</h2>
+                <button key={'2'} onClick={() => this.setState({counter: this.state.counter + 1})}>+</button>
+                <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+            </Auxiliary>
+        )
     }
 }
 
